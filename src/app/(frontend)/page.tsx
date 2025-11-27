94671f7a0f23ce654d8a9df8356c7305da1fac52
geofrey254 <devongeoffreymaina@gmail.com>
2025-11-27 12:09:51 +0300

    Added a Marquee Section

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '@/components/Homepage/Hero'
+import Jumbotron from '@/components/Homepage/Jumbotron'
 
 export default function page() {
   return (
     <>
       <Hero />
+      <Jumbotron />
     </>
   )
 }
---
+++ src/components/Homepage/Jumbotron.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+import { GoNorthStar } from "react-icons/go";
+
+export default function Jumbotron() {
+  return (
+    // macquee section
+    <section>
+      <div className="bg-[#1e254b] text-white py-4 overflow-hidden">
+        <div className="whitespace-nowrap flex space-x-8 md:space-x-16 px-4 md:px-8">
+          <Marquee className='flex gap-4'>
+            <span className="text-sm md:text-lg font-semibold flex items-center">
+              Welcome to Newman Ministries &nbsp; &nbsp; <GoNorthStar /> &nbsp; &nbsp;
+            </span>
+            <span className="text-sm md:text-lg font-semibold flex items-center">
+              Join us for Sunday Services at 10 AM &nbsp; &nbsp; <GoNorthStar /> &nbsp; &nbsp;
+            </span>
+            <span className="text-sm md:text-lg font-semibold flex items-center">
+              Get Involved in Our Community Programs &nbsp; &nbsp; <GoNorthStar /> &nbsp; &nbsp;
+            </span>
+            <span className="text-sm md:text-lg font-semibold flex items-center">
+              Support Our Mission &nbsp; &nbsp; <GoNorthStar /> &nbsp; &nbsp;
+            </span>
+            <span className="text-sm md:text-lg font-semibold flex items-center">Contact Us at: +254 707 019270 &nbsp; &nbsp; <GoNorthStar /> &nbsp; &nbsp;</span>
+          </Marquee>
+        </div>
+      </div>
+    </section>
+  )
+}
